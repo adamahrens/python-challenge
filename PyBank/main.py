4106d80f9a25c294b35125a1dcfad417851b9d5a
Adam Ahrens <adam.aahrens@gmail.com>
2019-09-12 13:52:44 -0500

    Trying to calculate average delta

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,16 +3,20 @@ import csv
 total_months = 0
 total_revenue = 0
 average_change = 0
+total_deleta_change_month_to_month = []
 profit_increase_date = "Now"
 profit_increase_revenue = 0
 profit_decrease_date = "Now"
 profit_decrease_revenue = 0
 
-# First row in the csv in the Column Name of Date and Profit
 with open('02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv', newline='') as f:
     reader = csv.reader(f)
+    # First row in the csv in the Column Name of Date and Profit
     # next will skip the Date and Column
     next(reader)
+
+    # Need to look at previous row when calculating profit increase/decrease
+    previous_row = None
 
     # Should be all the rows with actual data
     for row in reader:
@@ -21,10 +25,45 @@ with open('02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv',
 
         # List get the first and last element
         current_date = row[0]
-        current_revenue = row[-1]
+        current_revenue = int(row[-1])
+
+        # Calculate Profit Increase & Decrease
+        if profit_increase_date == "Now":
+            #Increasing
+            profit_increase_date = current_date
+            profit_increase_revenue = current_revenue
+            # Decreasing
+            profit_decrease_date = current_date
+            profit_decrease_revenue = current_revenue
+        else:
+            delta = current_revenue - int(previous_row[-1])
+
+            # Need a list of all the changes from month to month
+            total_deleta_change_month_to_month.append(delta)
+
+            # Is it a big profit?
+            if delta > profit_increase_revenue:
+                profit_increase_revenue = delta
+                profit_increase_date = current_date
+
+            # Is it a big loss?
+            if delta < profit_decrease_revenue:
+                profit_decrease_revenue = delta
+                profit_decrease_date = current_date
+
 
         # Update total_revenue
-        total_revenue += int(current_revenue)
+        total_revenue += current_revenue
+
+        # Update the previous rows
+        previous_row = row
+
+# Calculate average change from month to month
+total = 0
+for change in total_deleta_change_month_to_month:
+    total += change
+
+average_change = total / total_months
 
 print('Financial Analysis')
 print('----------------------------')
