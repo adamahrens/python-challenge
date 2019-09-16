7f2e6dbfa895a8a55d0807020768b1d4c0f4fc9e
Adam Ahrens <adam.aahrens@gmail.com>
2019-09-16 11:27:42 -0500

    Cleaning up code, ignoring .vscode file

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,34 +4,31 @@ total_months = 0
 total_revenue = 0
 average_change = 0
 total_deleta_change_month_to_month = []
-profit_increase_date = "Now"
+profit_increase_date = ""
 profit_increase_revenue = 0
-profit_decrease_date = "Now"
+profit_decrease_date = ""
 profit_decrease_revenue = 0
 
 with open('02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv', newline='') as f:
     reader = csv.reader(f)
-    # First row in the csv in the Column Name of Date and Profit
-    # next will skip the Date and Column
+    # First row in the csv in the Column Name of Date and Profit next will skip the Date and Column
     next(reader)
 
     # Need to look at previous row when calculating profit increase/decrease
-    previous_row = None
+    previous_row = []
 
-    # Should be all the rows with actual data
     for row in reader:
-        # Update total months
         total_months += 1
 
-        # List get the first and last element
+        # row is a List get the first (Date) and last (Revenue) element
         current_date = row[0]
-        current_revenue = int(row[-1])
+        current_revenue = int(row[-1]) # Casting from a String to Integer
 
         # Calculate Profit Increase & Decrease
         # Since its the first row of data
         # we need to to set the default values
         # we have nothing else to compare it to.
-        if profit_increase_date == "Now":
+        if len(profit_increase_date) == 0:
             #Increasing
             profit_increase_date = current_date
             profit_increase_revenue = current_revenue
@@ -55,7 +52,6 @@ with open('02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv',
             if delta < profit_decrease_revenue:
                 profit_decrease_revenue = delta
                 profit_decrease_date = current_date
-
 
         # Update total_revenue
         total_revenue += current_revenue
